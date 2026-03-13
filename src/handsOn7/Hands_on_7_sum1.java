0d3f1f6acea9171ef45668b39f6521133b42632a
haritha <haritharavichandran11@gmail.com>
2026-03-13 16:35:27 +0530

    chnages in handson 7

---
+++ src/handsOn7/Hands_on_7_sum1.java
@@ -0,0 +1,16 @@
+package handsOn7;
+
+import java.util.Scanner;
+
+public class Hands_on_7_sum1 {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter the string:");
+		String[] str;
+		
+	
+
+	}
+
+}
